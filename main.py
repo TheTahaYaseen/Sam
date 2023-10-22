561b626804c07673d230b1f6c3ae7429fe1a6457
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-22 05:01:18 -0700

    Defined Get Structured Time!

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # Schedule Announcer Mate
 
-# Get Structured Date Function
+# Imports
+import datetime
 
+
+# Get Structured Date Function
+def get_current_structure_date():
+    current_datetime = datetime.datetime.now()
+    current_day_of_the_month = current_datetime.strftime("%d")
+    current_month = current_datetime.strftime("%B")
+    current_year = current_datetime.strftime("%Y")
+    current_date = f"{current_day_of_the_month} {current_month} {current_year}"
+    return current_date
 
 # Getting Schedule Accordingly From Google Sheets
 
