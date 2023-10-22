5f4de4b9d90ff4df30f5310bfcc90924213d1b1c
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-22 04:56:46 -0700

    Pseudocode Of v0.0

---
+++ main.py
@@ -0,0 +1,21 @@
+# Schedule Announcer Mate
+
+# Get Structured Date Function
+
+
+# Getting Schedule Accordingly From Google Sheets
+
+
+# Current Date
+
+
+# Dictionary For Announcements With Their Times
+
+
+# Thread For Checking And Updating Date | Accordingly, Configuring The Dictionary
+
+
+# Thread For Schedule Announcement For The Day | Going Through The Dictionary, Checking For Each's Time
+
+
+# Main With Everything Compiled
